a8a9b1a2a3d9e74ceb0298bfaf69546c33788323
Nikola Menkovic 190/22 <nikola.menkovic.190.22@ict.edu.rs>
2026-04-08 14:59:03 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 
 int main() {
+    char ime[30];
+    char prezime[30];
+    char indeks[20];
+    int godina;
 
-    string godina;
+    printf("Unesite ime: ");
+    scanf("%s", ime);
 
-    printf("Ime i prezime: Nikola Menkovic ");
+    printf("Unesite prezime: ");
+    scanf("%s", prezime);
 
-    printf("Indeks: 190/22 ");
-    
-    printf("Koliko imate godina? :")
-    scanf_s("%s",&godina);
-    
-    /*
-     if (godina == prestupna)
-     printf("rodjen si u presupnoj godini")
+    printf("Unesite broj indeksa: ");
+    scanf("%s", indeks);
 
-    if (godina == nije prestupna)
-    printf("nisi rodjen u prestupnoj godini")
- */
-            
+    printf("Unesite godinu rodjenja: ");
+    scanf("%d", &godina);
+
+    printf("\n--- PODACI O STUDENTU ---\n");
+    printf("Ime i prezime: %s %s\n", ime, prezime);
+    printf("Broj indeksa: %s\n", indeks);
+    printf("Godina rodjenja: %d\n", godina);
+
+    if ((godina % 4 == 0 && godina % 100 != 0) || (godina % 400 == 0)) {
+        printf("Rodjeni ste u prestupnoj godini.\n");
+    } else {
+        printf("Niste rodjeni u prestupnoj godini.\n");
+    }
+
+    return 0;
+}
