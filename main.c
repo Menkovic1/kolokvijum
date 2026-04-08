7a6a70ffa9cb4407f3015c0c957c4c4eb885dc6f
Nikola Menkovic <nikola.menkovic.190.22@ict.edu.rs>
2026-04-08 14:34:42 +0200

    neki commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    string a, b, c;
+
+    printf("Unesite ime: ");
+    scanf("%d", &a);
+
+    printf("Unesite prezime: ");
+    scanf("%d", &b);
+
+    printf("broj indeksa: %d\n", c);
+
+    return 0;
+}
